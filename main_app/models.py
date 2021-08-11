30ed1b27f05a8db5eb3b9406d38e2f7039820b98
k-pura <85129355+k-pura@users.noreply.github.com>
2021-08-11 19:09:49 -0400

    fixes

---
+++ main_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Plant(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  description = models.TextField(max_length=1000)
+  culinary_use = models.TextField(max_length=1000)
+  medicinal_use = models.TextField(max_length=1000)
+  harvest_season = models.CharField(max_length=250)
+  cautions = models.TextField(max_length=250)
+
+
+class Recipe(models.Model):
+  name = models.CharField(max_length=100)
+  servings = models.IntegerField()
+  preptime = models.CharField(max_length=50)
+  cooktime = models.CharField(max_length=50)
+  ingredients = models.TextField(max_length=250)
+  directions = models.TextField(max_length=1000)
+  plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
